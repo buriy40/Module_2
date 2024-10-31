d14e65b0877ba235e1af4767cef1c454f74a0f7a
buriy40 <buriy40@bk.ru>
2024-10-31 22:39:25 +0300

    изменил приглашение в игру

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,2 +1,26 @@
-for i in range(0,10,2):
-    print(i)
+import random
+
+while True:
+    target_number = random.randint(0,10)
+    #print(target_number)
+    endeavours = 3
+    print('Угадай число от 1 до 10')
+    for endeavour in range(1,endeavours+1):
+        guess = int(input('введите число'))
+        if guess == target_number:
+            print('Верно!')
+            break
+        elif guess > target_number:
+            print('Заданное число меньше')
+        else:
+            print('Заданное число больше')
+        if endeavour == endeavours:
+            print('Ты проиграл')
+
+    play_agane = input('Еще разок? y/n')
+    if play_agane != 'y':
+        print('Спасибо за игру!')
+        break
+
+
+
