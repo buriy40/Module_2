a7d5c659f9689c0c92ec03dedf08387272cef26d
buriy40 <buriy40@bk.ru>
2024-11-04 10:21:41 +0300

    убрал квадратные скобки с value во второй матрице подставил m=0. все работает, возвращается пустой список

--- func.py
+++ func.py
@@ -3,12 +3,12 @@ def get_matrix(n, m, value):
     for i in range(n):
         matrix.append([])
         for j in range(m):
-            matrix[i].append([value])
+            matrix[i].append(value)
     if value <= 0:
         matrix = []
     return matrix
 
 
 print(get_matrix(2, 2, 10))
-print(get_matrix(3, 5, 42))
+print(get_matrix(3, 0, 42))
 print(get_matrix(4, 2, 13))
